a54f83ef9933831ec48031e30aa15d9b8973c293
hle223 <hieucong.le@dxc.com>
2023-08-03 15:14:12 +0700

    #1

---
+++ src/main/java/com/tms/config/WebSocketConfig.java
@@ -0,0 +1,40 @@
+package com.tms.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import com.tms.filters.WebSocketChannelInterceptor;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+	
+	
+	private final WebSocketChannelInterceptor wsInterceptor;
+	
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/ws-message")
+				.setAllowedOriginPatterns("*")
+				.withSockJS();
+	}
+	
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
+		registry.setApplicationDestinationPrefixes("/app");
+		registry.enableSimpleBroker("/topic", "/queue");
+	}
+	
+	@Override
+	public void configureClientInboundChannel(ChannelRegistration registration) {
+		registration.interceptors(wsInterceptor);
+	}
+
+}
